b95759dae8a0f9fd3162655e459a9238ddd9b1cf
Roman Karpovich <fpm.th13f@gmail.com>
2022-05-18 09:38:15 +0800

    format output as a table

--- calculate_bribes.py
+++ calculate_bribes.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-import requests
 import sys
-from dateutil.parser import parse
 
+import requests
+from dateutil.parser import parse
+from tabulate import tabulate
 
 my_aqua = int(sys.argv[1]) * 1000
-print('---------------------------------------------------------------------')
 stats_url = 'https://voting-tracker.aqua.network/api/voting-snapshot/stats/'
 bribes_url = 'https://bribes-api.aqua.network/api/bribes/?limit=20&page=1'
 votes_url = 'https://voting-tracker.aqua.network/api/voting-snapshot/'
@@ -25,9 +25,18 @@ for bribe in bribes_data.json()['results']:
     }
     distribution = []
     for ab in bribe['aggregated_bribes']:
-        distribution.append((ab['asset_code'], '{:.2f}'.format(float(ab['daily_aqua_equivalent']) / bribes[bribe['market_key']]['daily_amount_aqua'])))
+        distribution.append((
+            ab['asset_code'],
+            '{:.2f}'.format(float(ab['daily_aqua_equivalent']) / bribes[bribe['market_key']]['daily_amount_aqua'])
+        ))
 
-    bribes[bribe['market_key']]['distribution'] = list(reversed(sorted(distribution, key=lambda d: d[1])))
+    distribution = list(reversed(sorted(distribution, key=lambda d: d[1])))
+    bribes[bribe['market_key']]['distribution'] = distribution
+    if len(distribution) == 1:
+        distribution_text = distribution[0][0]
+    else:
+        distribution_text = ', '.join(['{}: {}'.format(*d) for d in distribution])
+    bribes[bribe['market_key']]['distribution_text'] = distribution_text
 
 votes_data = requests.get(votes_url + '?' + '&'.join(f'market_key={key}' for key in bribes.keys()))
 for vote in votes_data.json()['results']:
@@ -48,20 +57,38 @@ for bribe in bribes.values():
     bribe['percentage'] *= 1.5
     bribe['total_percentage'] *= 1.5
 
-
 for bribe in bribes.values():
     bribe['my_value'] = my_aqua / (bribe['votes'] + my_aqua) * bribe['daily_amount_aqua']
     bribe['my_share'] = my_aqua / bribe['votes']
 
+# filter out unavailable pairs
+bribes = {
+    key: bribe
+    for key, bribe in bribes.items()
+    if bribe.get('votes')
+}
 
-bribes = {key: bribe for key, bribe in bribes.items() if bribe.get('votes')} # filter out unavailable pairs
-bribes = {key: bribe for key, bribe in bribes.items() if bribe['total_percentage'] > 0.01} # filter out pairs out of reward zone
+# filter out of reward zone
+bribes = {
+    key: bribe
+    for key, bribe in bribes.items()
+    if bribe['total_percentage'] > 0.01
+}
 
 top_bribes = reversed(sorted(bribes.values(), key=lambda b: b['my_value']))
+result_table = []
 for bribe in top_bribes:
-    print('{}: profit {} AQUA, my share: {:.1f}%, {:.2f}% -> {:.2f}% of votes, stop at {:%Y-%m-%d}, distribution: {}'.format(
-        bribe['pair'], int(bribe['my_value']), bribe['my_share']*100,
-        bribe['percentage']*100, bribe['total_percentage']*100,
-        bribe['stop_at'], bribe['distribution']
-    ))
-print('---------------------------------------------------------------------')
+    result_table.append([
+        bribe['pair'], int(bribe['my_value']), bribe['my_share'] * 100,
+        '{:.2f} -> {:.2f}'.format(bribe['percentage'] * 100, bribe['total_percentage'] * 100),
+        bribe['stop_at'].strftime('%Y-%m-%d'),
+        bribe['distribution_text']
+    ])
+
+print(
+    tabulate(
+        result_table,
+        headers=['Pair', 'Profit (AQUA)', 'My share %', '% of votes', 'Stop at', 'Paid in'],
+        floatfmt=".2f"
+    )
+)
